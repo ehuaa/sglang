b67067acad675b229be1a293afdc9d477a5b91f3
ehuaa <ehuamail@163.com>
2025-08-20 16:33:52 +0800

    update start_sglang for gpt-oss-120b

--- start_sglang.sh
+++ start_sglang.sh
@@ -1,10 +1,10 @@
 docker run --gpus=all \
     --shm-size 32g \
-    --name sglang_test \
+    --name sglang_oss_test \
     -p 30012:30012 \
-    -e CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
+    -e CUDA_VISIBLE_DEVICES="0,1,2,3" \
     -v /nas:/nas \
     -v /data:/root/models \
     --ipc=host \
-    sglang:v0.4.6-post5 \
-    python3 -m sglang.launch_server --model-path /nas/xq/models/geogpt-r1/geogpt-q25-72b-cpt-r1-s2-dpo-0423 --tp 8 --trust-remote-code --host 0.0.0.0 --port 30012 --attention-backend fa3 --enable-mixed-chunk --reasoning-parser deepseek-r1
+    sglang:v0.4.10 \
+    python3 -m sglang.launch_server --model-path /nas/xq/models/gpt-oss-120b --tp 2 --enable-mixed-chunk --mem-fraction-static 0.93 --host 0.0.0.0 --port 30012
